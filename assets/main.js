ecf288c37dfd1f12b143eaca234720418f127a2c
hehe1111 <2908749709@qq.com>
2021-01-05 00:35:01 +0800

    feat: 图片懒加载；复制图片链接；使用 scss

--- assets/main.js
+++ assets/main.js
@@ -5,28 +5,65 @@ const images = JSON.parse(
 new Vue({
   el: '#app',
   data: {
-    images
+    images,
+    copiedLink: '',
+    timerId: ''
+  },
+  mounted() {
+    this.handleLazyLoading()
+    this.handleCopy()
+  },
+  methods: {
+    handleLazyLoading() {
+      const lazyImages = [].slice.call(document.getElementsByTagName('img'))
+      const wHeight = window.innerHeight
+
+      const lazyLoading = () => {
+        const loadedIndexes = []
+        lazyImages.map((i, index) => {
+          if (!i) return
+          const { top: eTop } = i.getBoundingClientRect()
+          if (eTop < wHeight) {
+            i.setAttribute('src', i.getAttribute('data-src'))
+            loadedIndexes.push(index)
+          }
+        })
+
+        loadedIndexes.map(i => (lazyImages[i] = undefined))
+      }
+
+      window.addEventListener('scroll', lazyLoading)
+      this.$once('hook:beforeDestroy', () => {
+        window.removeEventListener('scroll', lazyLoading)
+      })
+      lazyLoading()
+    },
+    handleCopy() {
+      const onCopy = event => {
+        event.clipboardData.setData(
+          'text/plain',
+          `${window.location.origin}/image-host${this.copiedLink}`
+        )
+        event.preventDefault()
+      }
+      document.addEventListener('copy', onCopy)
+      this.$once('hook:beforeDestroy', () => {
+        document.removeEventListener('copy', onCopy)
+      })
+    },
+    copyLink(imagePath) {
+      this.copiedLink = imagePath
+      document.execCommand('copy')
+      const { className, classList } = this.$refs.copySuccess
+      if (!className.includes('show')) return this.toggleCopySuccess()
+      clearTimeout(this.timerId)
+      classList.remove('show')
+      setTimeout(this.toggleCopySuccess, 400)
+    },
+    toggleCopySuccess() {
+      const { classList } = this.$refs.copySuccess
+      classList.add('show')
+      this.timerId = setTimeout(() => classList.remove('show'), 2000)
+    }
   }
 })
-
-/* === 懒加载图片 === */
-
-const lazyImages = [].slice.call(document.getElementsByTagName('img'))
-const wHeight = window.innerHeight
-
-function lazyLoading() {
-  const loadedIndexes = []
-  lazyImages.map((i, index) => {
-    if (!i) return
-    const { top: eTop } = i.getBoundingClientRect()
-    if (eTop < wHeight) {
-      i.setAttribute('src', i.getAttribute('data-src'))
-      loadedIndexes.push(index)
-    }
-  })
-
-  loadedIndexes.map(i => (lazyImages[i] = undefined))
-}
-
-window.onscroll = lazyLoading
-lazyLoading()
